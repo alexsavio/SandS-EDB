eabdcc18ea8962ff1d5a4b24e286abca9a03ddb0
Alexandre M. S. <alexsavio@gmail.com>
2014-02-04 00:08:51 +0100

    Update mail.py

--- src/mail.py
+++ src/mail.py
@@ -6,5 +6,5 @@ mail['MAIL_SERVER'] = 'smtp.gmail.com'
 mail['MAIL_PORT'] = 465
 mail['MAIL_USE_SSL'] = True
 mail['MAIL_USERNAME'] = 'alexsavio'
-mail['MAIL_PASSWORD'] = 'I#Alex@Savio'
+mail['MAIL_PASSWORD'] = '*******'
 #mail = Mail(app)
