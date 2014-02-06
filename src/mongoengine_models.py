6b92880478aaf4ce0d4e872392bb549637affdb5
Borja Ayerdi <ayerdi.borja@gmail.com>
2014-02-06 18:50:22 +0100

    Update mongoengine_models.py

--- src/mongoengine_models.py
+++ src/mongoengine_models.py
@@ -32,7 +32,7 @@ class DayHourField(StringField):
 class Preference(Document):
     criterion = StringField()
     value = StringField(choices=('agree', 'neutral', 'disagree', 'partialAgree', 'partialDisagree'))
-
+    
 # Parameter
 class Parameter(Document):
     name = StringField()
@@ -43,93 +43,54 @@ class Evaluation(Document):
     criterion = StringField()
     value = StringField(choices=('optimum', 'good', 'neutral', 'bad', 'verybad'))
 
-# Warningmsg
-class Warningmsg(Document):
+# Warning
+class Warning(Document):
     message = StringField()
     instructionNum = IntField()
-
-# Request
-class Request(Document):
-    # Daydate
-    date = DayDateField()
-    action = StringField()
-    # dayhour
-    time = DayHourField()
-    status = StringField(choices=('waitingRecipe', 'waitingForExecution', 'executing', 'feedback', 'error', 'complete'))
-    Parameters = ListField(ReferenceField(Parameter))
-    Evaluations = ListField(ReferenceField(Evaluation))
-    Warningmsgs = ListField(ReferenceField(Warningmsg))
-
-# Eahouker
-class Eahouker(Document):
-    userName = StringField(max_length=50, required=True, unique=True)
-    pwd = StringField(max_length=50, required=True)
-    # Daydate
-    birthday = DayDateField()
-    sex = StringField(choices=('male', 'female', 'gay', 'lesbian', 'shemale', 'neutral', 'chaste', 'NA'))
-    numChildren = IntField()
-    socialStatus = StringField(choices=('single', 'married', 'separated'))
-    Preference = ListField(ReferenceField(Preference))
-    Request = ListField(ReferenceField(Request))
 
 # Instruction
 class Instruction(Document):
     name = StringField()
     value = StringField()
-    type = StringField()
-    duration = IntField()
 
 # Recipe
 class Recipe(Document):
     numExecutions = IntField()
     qualityIndex = FloatField()
     isBasic = BooleanField()
-    Requests = ListField(ReferenceField(Request))
     Instructions = ListField(ReferenceField(Instruction))
-
-# InstructionForAppliance
-class InstructionForAppliance(Document):
-    Instruction = ReferenceField(Instruction))
-    range = DictField(default={'range':'', 'accuracy':''})
-
-# BasicRecipe
-class BasicRecipe(Document):
-    Instructions = ListField(ReferenceField(Instruction))
-
-# ApplianceModel
-class ApplianceModel(Document):
-    brand = StringField()
-    model = StringField()
-    certification = StringField()
-    consume = DictField(default={'gas':0, 'power':0,'water':0,'temperature':0})
-    actions = ListField(StringField())
-    criteria = ListField(StringField())
-    technicalData = DictField(default={'power':'', 'inletWaterFlow':'','maxInletWaterPressure':''})
-    exceptions = ListField(StringField())
-    events = ListField(StringField())
-    BasicRecipes = ListField(ReferenceField(BasicRecipe))
 
 # Manufacturer
 class Manufacturer(Document):
     userName = StringField(unique=True)
     pwd = StringField()
     url = StringField()
-    ApplianceModels = ListField(ReferenceField(ApplianceModel))
 
-# EngagedAppliance
-class EngagedAppliance(Document):
-    ipAddress = StringField()
-    status = StringField()
-    encrKey = StringField()
-    ApplianceModel = ReferenceField(ApplianceModel)
-    Requests = ListField(ReferenceField(Request))
 
-# Location
-class Location(Document):
-    address = DictField(default={'street':'', 'country':'','position':'','number':'','city':''})
-    ipAddress = StringField()
-    EngagedAppliances = ListField(ReferenceField(EngagedAppliance))
-    Eahoukers = ListField(ReferenceField(Eahouker))
+# BasicRecipe
+class BasicRecipe(Document):
+    Instructions = ListField(ReferenceField(Instruction))
+
+# InstructionType
+class InstructionType(Document):
+    name = StringField()
+    continuous = DictField(default={'minValue':0, 'maxValue':0,'step':0,'defaultValue':0})
+    discrete = ListField(StringField())
+
+# ApplianceModel
+class ApplianceModel(Document):
+    brand = StringField()
+    model = StringField()
+    certification = StringField()
+    Manufacturer = ReferenceField(Manufacturer)
+    consume = DictField(default={'gas':0, 'power':0,'water':0,'temperature':0})
+    TechnicalData = DictField(default={'power':'', 'inletWaterFlow':'','maxInletWaterPressure':''})
+    actions = ListField(StringField())
+    criteria = ListField(StringField())
+    exceptions = ListField(StringField())
+    events = ListField(StringField())
+    BasicRecipes = ListField(ReferenceField(BasicRecipe))
+    InstructionTypes = ListField(ReferenceField(InstructionType))
 
 # HomeRule
 class HomeRule(Document):
@@ -140,11 +101,45 @@ class HomeRule(Document):
 class HomeRuleFormat(Document):
     parameter = StringField()
 
-# InstructionDefault
-class InstructionDefault(Document):
-    type = IntField()
-    name = StringField()
-    rangeContinuous = DictField(default={'minValue':0, 'maxValue':0,'step':0,'defaultValue':0})
-    # rangeDiscrete?
-    rangeDiscrete = ListField(IntField())
+# Location
+class Location(Document):
+    address = DictField(default={'street':'', 'country':'','position':'','number':'','city':''})
+    ipAddress = StringField()
+    HomeRules = ListField(ReferenceField(HomeRule))
+    HomeRuleFormats = ListField(ReferenceField(HomeRuleFormat))
+
+# EngagedAppliance
+class EngagedAppliance(Document):
+    ipAddress = StringField()
+    status = StringField()
+    encrKey = StringField()
+    ApplianceModel = ReferenceField(ApplianceModel)
+    Location = ReferenceField(Location)
+    #Requests = ReferenceField(Request)
+
+
+# Request
+class Request(Document):
+    date = DayDateField()
+    action = StringField()
+    time = DayHourField()
+    status = StringField(choices=('waitingRecipe', 'waitingForExecution', 'executing', 'feedback', 'error', 'complete'))
+    Parameters = ListField(ReferenceField(Parameter))
+    Evaluations = ListField(ReferenceField(Evaluation))
+    Warnings = ListField(ReferenceField(Warning))
+    Recipe = ReferenceField(Recipe)
+    EngagedAppliance = ReferenceField(EngagedAppliance)
+
+# Eahouker
+class Eahouker(Document):
+    userName = StringField(max_length=50, required=True, unique=True)
+    pwd = StringField(max_length=50, required=True)
+    birthday = DayDateField()
+    sex = StringField(choices=('male', 'female', 'gay', 'lesbian', 'shemale', 'neutral', 'chaste'))
+    numChildren = IntField()
+    socialStatus = StringField(choices=('single', 'married', 'separated'))
+    Preference = ListField(ReferenceField(Preference))
+    Request = ListField(ReferenceField(Request))
+    Location = ListField(ReferenceField(Location))
+
 
